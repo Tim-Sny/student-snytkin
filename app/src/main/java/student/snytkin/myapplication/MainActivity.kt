33f7ed49a5dcb23cefee4316c4840c292f328854
Tim-Sny <t.snytkin@mail.ru>
2021-11-30 23:27:17 +0700

    Android Styles

--- app/src/main/java/student/snytkin/myapplication/MainActivity.kt
+++ app/src/main/java/student/snytkin/myapplication/MainActivity.kt
@@ -1,11 +1,24 @@
 package student.snytkin.myapplication
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import java.security.KeyStore
 
+const val TAG = "Poem"
 class MainActivity : AppCompatActivity() {
+
+    lateinit var nextActivityButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
 }
